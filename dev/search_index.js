d8876ec109fb691168521e2491109dcf63d2195c
Documenter.jl <documenter@juliadocs.github.io>
2021-10-21 02:30:26 +0000

    build based on 85c5596

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageZW","category":"page"},{"location":"#DemoPackageZW","page":"Home","title":"DemoPackageZW","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageZW.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageZW]","category":"page"}]
+}
